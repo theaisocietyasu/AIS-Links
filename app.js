5b039c8e5a725422d16435b79c78192e77f1656e
The AI Society <148732241+theaisocietyasu@users.noreply.github.com>
2023-12-22 05:58:41 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const shareButtons = document.querySelectorAll('.tile-share-button')
+
+// let toastContainer;
+
+// function generateToast({message}){
+//     toastContainer.insertAdjacentHTML('beforeend', `p class="toast">${message}</p>`)
+// }
+
+// (function initToast() {
+//     document.body.insertAdjacentHTML('afterbegin', `<div class="toast-container"></div>`)
+//     toastContainer = document.querySelector('.toast-container')
+// })()
+// shareButtons.addEventListener('click', () => {
+//     generateToast({
+//         message: 'Link Copied to Clipboard!', 
+//     })
+// })
+
+
+async function copyText(e) {
+    e.preventDefault()
+    const link = this.getAttribute('link')
+    try {
+        await navigator.clipboard.writeText(link)
+        alert("Link Copied to Cliboard!\n" + link)
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+shareButtons.forEach(shareButton => shareButton.addEventListener('click', copyText))
+
