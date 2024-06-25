bcaf4fda2e92d43c924fba62992a37a63e86813d
Siddhesh Badani <sid13@asu.edu>
2024-06-25 19:22:36 +0530

    cleaned up the code. got rid of unwanted, commented code bits

--- app.js
+++ app.js
@@ -1,26 +1,5 @@
 const shareButtons = document.querySelectorAll('.tile-share-button')
 
-// let toastContainer;
-
-// function generateToast({message}){
-//     toastContainer.insertAdjacentHTML('beforeend', `p class="toast">${message}</p>`)
-// }
-
-// (function initToast() {
-//     document.body.insertAdjacentHTML('afterbegin', `<div class="toast-container"></div>`)
-//     toastContainer = document.querySelector('.toast-container')
-// })()
-// shareButtons.addEventListener('click', () => {
-//     generateToast({
-//         message: 'Link Copied to Clipboard!', 
-//     })
-// })
-
-const bgvideo = document.querySelector('.background-video')
-bgvideo.muted = "true"
-bgvideo.play()
-
-
 async function copyText(e) {
     e.preventDefault()
     const link = this.getAttribute('link')
